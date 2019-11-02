72d875ff110393a04985648b146a9a2d8baeec8b
hn4729 <henry0118@utexas.edu>
2019-11-02 17:24:52 -0500

    setting up django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import dotenv
+
+dotenv.load_dotenv()
+
+USER_PASSWORD = os.getenv("USER_PASSWORD")
+HOST = os.getenv("HOST")
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +27,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9@o0&)yv&f7asyksx7tgv^y9xfkks&c%=0u8w44go&7(hitz*l'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pokedex_app',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +86,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'hn4729',
+        'HOST': HOST,
+        'USER': 'hn4729',
+        'PASSWORD': USER_PASSWORD
     }
 }
 
